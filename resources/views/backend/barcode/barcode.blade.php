8bf483743146f905a140aa0aa2a0b72df024a4bc
= <m.talha@geekybugs.com>
2023-08-19 12:58:47 +0500

    barcoce update

--- resources/views/backend/barcode/barcode.blade.php
+++ resources/views/backend/barcode/barcode.blade.php
@@ -26,23 +26,21 @@
 <div class="container mt-5">
     <div class="row d-flex" id="product-cards">
         @foreach ($products as $product )
-        <div class="col-md-5">
+        <div class="col-md-4">
             <div class="card mb-3 shadow-lg p-3 ">
                 <div class="card-body">
                     <h5 class="card-title">Name: {{$product->name}}</h5>
-                    <!-- <span>{!! DNS1D::getBarcodeHTML($product->product_id . "123", "C128", 1.4, 22) !!}</span> -->
                     @foreach ($product->productVariants as $variant )
                     <p class="card-text"><strong>Price:</strong> {{$variant->price}}</p>
                     <p>{{$product->id.",".$variant->barcode}}</p>
                     @if (!empty($variant->barcode))
                     <?php
                     // echo '<img src="data:image/png;base64,' . DNS1D::getBarcodePNG($variant->id, "C128", 1.4, 22) . '" alt="barcode"   />';
-                    echo '<img src="data:image/png;base64,' . DNS1D::getBarcodePNG($product->id.",".$variant->barcode, "C128", 1, 22) . '" alt="barcode" width="250" />';
+                    echo '<img src="data:image/png;base64,' . DNS1D::getBarcodePNG($product->id . "," . $variant->barcode, "C128", 1, 22) . '" alt="barcode" width="250" />';
                     // echo '<img src="data:image/png;base64,' . DNS1D::getBarcodePNG($product->code.",".$variant->barcode, "C128", 1, 22) . '" alt="barcode" width="250" height="20"   />';
                     ?>
-                    
+
                     @endif
-                    <!-- <h5 class="card-title">Title: {{$variant->barcode}}</h5> -->
                     @endforeach
                 </div>
             </div>
